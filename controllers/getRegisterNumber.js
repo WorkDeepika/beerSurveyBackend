cfa4ac30cfa686d4ddf14f0477d2a2eea423887a
deepika8383 <jaindeepika8383@gmail.com>
2025-09-01 00:53:29 +0530

    bnvhdf

--- controllers/getRegisterNumber.js
+++ controllers/getRegisterNumber.js
@@ -1,41 +1,5 @@
 const PanelCount = require('../models/panelCountModel');
 
-// const getRegisterNumber = async (req, res) => {
-//   try {
-//     const panelId = parseInt(req.params.panelId, 10);
-//     const { isTest } = req.body;  // or req.body, depending on how you send it
-
-//     if (panelId < 1 || panelId > 6) {
-//       return res.status(400).json({ error: "Invalid panel ID" });
-//     }
-
-//     if (isTest === "true") {
-//       // Just return current number without incrementing
-//       const panel = await PanelCount.findOne({ panelId });
-//       if (!panel) {
-//         return res.status(404).json({ error: "Panel not found" });
-//       }
-//       return res.json({ registrationNumber: panel.count +1 });
-//     }
-
-//     // Normal flow: atomic increment but only if count < 100
-//     const panel = await PanelCount.findOneAndUpdate(
-//       { panelId, count: { $lt: 100 } },
-//       { $inc: { count: 1 } },
-//       { new: true }
-//     );
-
-//     if (!panel) {
-//       return res
-//         .status(400)
-//         .json({ error: "Maximum registrations reached for this panel" });
-//     }
-
-//     return res.json({ registrationNumber: panel.count });
-//   } catch (error) {
-//     return res.status(500).json({ error: error.message });
-//   }
-// };
 const getRegisterNumber = async (req, res) => {
   try {
     const panelId = parseInt(req.params.panelId, 10);
@@ -44,9 +8,9 @@ const getRegisterNumber = async (req, res) => {
     // Panel-specific limits
     const panelLimits = {
       1: 148,
-      2: 136,
-      3: 125,
-      4: 128,
+      2: 140,
+      3: 129,
+      4: 131,
       5: 123,
       6: 108,
     };
